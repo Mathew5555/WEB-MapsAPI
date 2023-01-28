81b767d97395721ec4230f132d6f46db970c7037
HappyMatDuckling <matveyd05@yandex.ru>
2023-01-28 14:02:26 +0300

    Большая задача по Maps API. Часть №2

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys
 from io import BytesIO
 
 import requests
-from PyQt5.QtGui import QPixmap, QImage
+
+from PyQt5.QtGui import QPixmap, QImage, QKeyEvent
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
 from PyQt5 import uic
+from PyQt5.Qt import *
 
 SCREEN_SIZE = [600, 450]
 
@@ -20,7 +22,6 @@ class Map(QMainWindow):
         self.map_ll = [37.9777751, 55.757718]
         self.map_l = "map"
         self.refresh_map()
-        # map_request = f"http://static-maps.yandex.ru/1.x/?ll={map_ll}&l={map_l}&"
 
     def refresh_map(self):
         map_params = {
@@ -32,10 +33,20 @@ class Map(QMainWindow):
         response = requests.get("http://static-maps.yandex.ru/1.x/", params=map_params)
         self.image.setPixmap(QPixmap.fromImage(QImage.fromData(response.content)))
 
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == Qt.Key_Up and self.map_zoom < 18:
+            self.map_zoom += 1
+        elif key == Qt.Key_Down and self.map_zoom > 0:
+            self.map_zoom -= 1
+        self.refresh_map()
+
     def initUI(self):
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
+        layout = QVBoxLayout()
+        layout.addWidget(self.image, alignment=Qt.AlignCenter)
 
 
 if __name__ == '__main__':
